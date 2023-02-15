ceb79f7c0e1be17c7135b83890005130a899ab67
Владислав Сущик <@nchtdm.loc>
2023-02-15 12:11:51 +0300

    task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    rez = n.toString()
+    if (rez[0]==='-'){
+      rez = rez.slice(1,rez.length)
+    }
+    console.log(rez)
+    arr = rez.split('')
+    arr = arr.reverse()
+    rez1 = arr.join('')
+    rez1 = Number(rez1)
+    return(rez1)
 }
